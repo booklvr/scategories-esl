a44526a985aaf1d38f58d1dac81f49a2ba7bdf4a
Nicholas de Waal <nick.a.dewaal@gmail.com>
2021-02-01 08:47:39 +0900

    added route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 // import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { HashRouter as Router } from 'react-router-dom'
+import { HashRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
